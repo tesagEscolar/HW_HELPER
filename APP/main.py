9765fe1c5d25e321b040e99314c7fd3f67730a6a
tesagEscolar <113012275+tesagEscolar@users.noreply.github.com>
2025-03-09 05:08:17 -0600

    Update main.py

--- APP/main.py
+++ APP/main.py
@@ -1,7 +1,8 @@
 from Jarvis import Jarvis
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":   
+    print('Running Jarvis')
     jv = Jarvis()
     jv.run(complex=True, mode='auto')
     
